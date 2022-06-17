6de6d9121c163a253fbcc6b61faa483948a32a05
adhl77 <hgj2482@naver.com>
2022-06-17 11:03:30 +0900

    three commit multi-sub-admin modify

---
+++ multi-sub-admin/src/main/java/com/multisub/controller/MainController.java
@@ -0,0 +1,16 @@
+package com.multisub.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+	
+	@RequestMapping("/")
+	public String main(Model m) {
+		m.addAttribute("center","center");
+		return "main";
+	}
+	
+}
